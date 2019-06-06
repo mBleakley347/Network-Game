a5686f84f60302db37d22c6d4fe37a8eed628cb2
Michael <1013532@student.sae.edu.au>
2019-06-06 14:18:02 +1000

    MB Mirror testing started

---
+++ Sins Game/Assets/GAME/Scripts/NetManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class NetManager : NetworkManager
+{
+
+    public Transform SpawnLocation;
+    public GameObject[] Sins;
+    public GameObject Hider;
+
+    public override void OnServerAddPlayer(NetworkConnection conn, AddPlayerMessage extraMessage)
+    {
+        Transform start = SpawnLocation;
+        GameObject player = Instantiate(Hider, start.position, start.rotation);
+        NetworkServer.AddPlayerForConnection(conn, player);
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        base.OnServerDisconnect(conn);
+    }
+}
