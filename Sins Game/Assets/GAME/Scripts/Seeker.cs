47e64ab48d62ccf5c793b6e3bacc0b1b02e11346
Michael Bleakley <1013532@student.sae.edu.au>
2019-06-13 12:27:32 +1000

    MB Seeker Movement Added

--- Sins Game/Assets/GAME/Scripts/Seeker.cs
+++ Sins Game/Assets/GAME/Scripts/Seeker.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Seeker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
+    public float speedMultiplier;
+    public float rotationSpeed;
+    [SerializeField] private float maxSpeed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Rotate();
     }
+
+    public void Move()
+    {
+        rb.AddRelativeForce(Vector3.forward * (Input.GetAxis("Vertical") * speedMultiplier),ForceMode.VelocityChange);
+        //changed from velocity to incorperate a max speed 
+    }
+
+    public void Rotate()
+    {
+        
+        rb.AddRelativeTorque(0,Input.GetAxis("Horizontal") * rotationSpeed,0,ForceMode.VelocityChange);
+       
+    } 
 }
