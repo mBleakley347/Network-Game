ddfef5af1a676f21054f3e2ed9afc345b9c88cb7
Michael <1013532@student.sae.edu.au>
2019-06-19 10:58:52 +1000

    MB Update to scripts

---
+++ Sins Game/Assets/GAME/V0.0.2 Tag/Scripts/CharacterBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+namespace Tag
+{
+    /*
+     * Wall Jump Ability
+     * 
+     */
+    public class CharacterBase : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        [Command]
+        public void cmdSpawn()
+        {
+            
+        }
+    }
+}
---
+++ Sins Game/Assets/GAME/V0.0.2 Tag/Scripts/SeekerScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tag
+{
+    /*
+     * Collision to end game
+     * 
+     */
+    public class SeekerScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.tag.Equals("Hider"))
+            {
+                
+            }
+        }
+    }
+}
